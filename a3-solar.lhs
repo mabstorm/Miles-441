6a45a7bd069beaf63039b96463b325191f736525
mabstorm <mabstorm@yahoo.com>
2011-11-13 21:33:17 -0500

    changed to clouds now

--- a3-solar.lhs
+++ a3-solar.lhs
@@ -1,12 +1,5 @@
 COS 441 Homework 3
 
-This is a literate Haskell file. It must type check and compile when you
-submit it.
-
-Note, on windows, ghci -i./SOE/src doesn't work. 
-I used ghci -iSOE/src to extend the ghci search path so it includes
-the SOE libraries (where SOE is a subdirectory containing the
-SOE libraries).
 
 > import Control.Applicative
 > import Animation hiding (planets, translate)
@@ -40,8 +33,6 @@ This part of the homework involves creating a model solar system.
 > sun :: Behavior Picture
 > sun = reg (lift0 Yellow) (shape (circ 1))
 
-The following define the main action of the solar system simulator. 
-You'll want to replace the right-hand side of planets with your solar system.
 
 > planets :: Behavior Picture
 > planets = sun
@@ -91,9 +82,6 @@ Next, use the provided function translateB to write a function
 > at (Beh f) t = f t
 
 
->--- orbit p1 p2 freq xrad yrad = lift2 Over p1 p2
->---   where p3 = translateB ( lift0(xrad*cos((fromIntegral(time))*freq)), lift0(yrad*sin((fromIntegral(time))*freq))) p2
-
 > orbit p1 p2 freq xrad yrad = overB p1 translated_p
 >      where translated_p = (translateB (floatx, floaty) p2)
 >            floatx :: Behavior Float
@@ -123,58 +111,11 @@ Next, use the provided function translateB to write a function
 > sun2 = reg (lift0 Yellow) (shape (circ 2.0))
 
 
-
-
-that takes two picture behaviors and makes the first orbit around the second at the specified 
-distance and with the specified radii. That is, the two pictures will be overlayed (using over) 
-and, at each time t, the position of the satellite will be translated by xradius * cos(t * frequency) 
-in the x dimension and by yradius * sin(t * frequency) in the y dimension.
-
-
-Test your function by creating another circle, mercury, colored red and with radius 0.1, and 
-making it orbit around the sun with a frequency of 2.0, and with radii of 2.0 and 0.2 in the 
-x and y axes, respectively.
-
-
-
-
 > -- running this definition in ghci should create the appropriate animation
 > orbitTest = do animateB "Solar system" (orbit sun mercury 2.0 2.0 0.2)
-
-
-
-A problem you might have noticed is the overlay behavior of planets. For this part modify 
-orbit to put planets over or under each other. 
-
-
-
-
 > -- running this definition in ghci should create the next animation
 > orbitTest' = do animateB "Solar system" (orbit_depth sun mercury 2.0 2.0 0.2)
-
-
-
-
-
-Modify your functions (and write any support functions that you find necessary) to make the orbital 
-distances and planet sizes shrink and grow by some factor (you can pass this factor as parameter 
-to the orbit function), according to how far the planets are from the observer. For example, the 
-earth and moon should look a little smaller when they are going behind the sun, and the orbital 
-distance of the moon from the earth should be less.
-
-
-
-
-
 > -- running this definition in ghci should create the next animation
 > orbitTest'' = do animateB "Solar system" (orbit_size sun2 2.0 3.1 0.2)
 
-
-
-
-Choose the scaling factor so that the solar system simulation looks good to you.
-Optional: Add some other planets, perhaps with their own moons. If you like, feel 
-free to adjust the parameters we gave above to suit your own aesthetic or astronomical 
-tastes. Make sure, though, that the features requested in previous parts - growing, 
-shrinking, occlusion, etc. - remain clearly visible.
 
--- movie.hs
+++ movie.hs
@@ -45,60 +45,121 @@ instance Applicative Behavior where
 --         spaceClose w
 
 overB = lift2 Over
-overP freq p1 p2 = Beh (\t ->
-                         if sin(t*freq) > 0
+overP time_off p1 p2  = Beh (\t ->
+                         if sin(t*(freq+time_off)) > 0
                              then at (overB p1 p2) t
                              else at (overB p2 p1) t
                        )
 at :: Behavior a -> Time -> a
 at (Beh f) t = f t
 
-ground_move :: Behavior Picture -- p1
-      -> Behavior Picture -- p1b
-      -> Behavior Picture -- p2
-      -> Behavior Picture -- p2b
-      -> Behavior Picture -- p3
-      -> Behavior Picture -- p3b
-      -> Float            -- y1
-      -> Float            -- y2
-      -> Float            -- y3
-      -> Float            -- freq
-      -> Float            -- xrad
-      -> Behavior Picture
 
-ground_move p1 p2 p3 p1b p2b p3b y1 y2 y3 freq xrad = (overP freq p1' p1b') `overB` (overP freq p2' p2b') `overB` (overP freq p3' p3b')  
-     where p1' = (translateB (x1, by1) p1)
-           p2' = (translateB (floatx, by2) p2)
-           p3' = (translateB (floatx, by3) p3)
-           p1b' = (translateB (x1, by1) p1b)
-           p2b' = (translateB (floatx, by2) p2b)
-           p3b' = (translateB (floatx, by3) p3b)
-           x1 :: Behavior Float
-           x1 = Beh (\t -> xrad*cos(t*freq)+1)
-           floatx :: Behavior Float
-           floatx = Beh (\t -> xrad*cos (t*freq))
-           by1 :: Behavior Float
-           by1 = Beh (\t -> y1)
-           by2 :: Behavior Float
-           by2 = Beh (\t -> y2)
-           by3 :: Behavior Float
-           by3 = Beh (\t -> y3)
+create_cloud :: Float -> Float -> Float -> Behavior Picture
+
+create_cloud cx cy time_off = (overP time_off p1 p1b) `overB` (overP time_off p2 p2b) `overB` (overP time_off p3 p3b) `overB` (overP time_off p4 p4b)
+  where p1 = (translateB (rightx, centery) ell1)
+        p2 = (translateB (centerx, topy) ell1)
+        p3 = (translateB (centerx, bottomy) ell1)
+        p4 = (translateB (leftx, centery) ell1)
+        p1b = (translateB (rightx, centery) ell1b)
+        p2b = (translateB (centerx, topy) ell1b)
+        p3b = (translateB (centerx, bottomy) ell1b)
+        p4b = (translateB (leftx, centery) ell1b)
+--       rightx :: Behavior Float
+--       leftx :: Behavior Float
+--       centerx :: Behavior Float
+--       topy :: Behavior Float
+--       bottomy :: Behavior Float
+--       centery :: Behavior Float
+        rightx = Beh (\t -> xrad*cos(t*(freq+time_off))+half_xrad+cx)
+        leftx = Beh (\t -> xrad*cos(t*(freq+time_off))-half_xrad+cx)
+        centerx = Beh (\t -> xrad*cos(t*(freq+time_off))+cx)
+        topy = Beh (\t -> cy + half_rad)
+        bottomy = Beh (\t -> cy - half_rad)
+        centery = Beh (\t -> cy)
+
+
 
 cw :: Color
 cw = White
 cb :: Color
 cb = Black
 
-ell1 = reg (lift0 cw) (shape (ell 1 0.1))
-ell2 = reg (lift0 cw) (shape (ell 1 0.1))
-ell3 = reg (lift0 cw) (shape (ell 1 0.1))
-ell1b = reg (lift0 cb) (shape (ell 1 0.1))
-ell2b = reg (lift0 cb) (shape (ell 1 0.1))
-ell3b = reg (lift0 cb) (shape (ell 1 0.1))
+ell_rad :: Behavior Radius
+ell_rad = 0.2
+half_rad :: Float
+half_rad = 0.1
+ell_xrad :: Behavior Radius
+ell_xrad = 0.5
+half_xrad :: Float
+half_xrad = 0.4
+
+
+
+ell1 = reg (lift0 cw) (shape (ell ell_xrad ell_rad))
+ell1b = reg (lift0 cb) (shape (ell ell_xrad ell_rad))
+
+freq :: Float
+freq = 0.1
+xrad :: Float
+xrad = 10
 
 main =
-  do animateB "Ground" (ground_move ell1 ell2 ell3 ell1b ell2b ell3b 0 (-2.0) 2 1 4)
+  --do animateB "clouds" (cloud_move ell1 ell2 ell3 ell1b ell2b ell3b 1.9 1.8 2 1 4)
+    do animateB "clouds" cloudblock where
+      cloudblock = cloud1 `overB` cloud2 `overB` cloud3 `overB` cloud4 where
+        cloud1 = create_cloud 4.9 1 0.1
+        cloud2 = create_cloud 0.3 2 0.1
+        cloud3 = create_cloud 1.5 0 0.2
+        cloud4 = create_cloud 2.9 1.3 0.3
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--cloud_move :: Behavior Picture -- p1
+--     -> Behavior Picture -- p1b
+--     -> Behavior Picture -- p2
+--     -> Behavior Picture -- p2b
+--     -> Behavior Picture -- p3
+--     -> Behavior Picture -- p3b
+--     -> Behavior Picture -- p3b
+--     -> Behavior Picture -- p3b
+--     -> Float            -- y1
+--     -> Float            -- y2
+--     -> Float            -- y3
+--     -> Float            -- freq
+--     -> Float            -- xrad
+--     -> Behavior Picture
+--
+--cloud_move p1 p2 p3 p4 p1b p2b p3b y1 y2 y3 freq xrad = (overP freq p1' p1b') `overB` (overP freq p2' p2b') `overB` (overP freq p3' p3b')  
+--    where p1' = (translateB (x1, by1) p1)
+--          p2' = (translateB (floatx, by2) p2)
+--          p3' = (translateB (floatx, by3) p3)
+--          p1b' = (translateB (x1, by1) p1b)
+--          p2b' = (translateB (floatx, by2) p2b)
+--          p3b' = (translateB (floatx, by3) p3b)
+--          x1 :: Behavior Float
+--          x1 = Beh (\t -> xrad*cos(t*freq)+1)
+--          floatx :: Behavior Float
+--          floatx = Beh (\t -> xrad*cos (t*freq))
+--          by1 :: Behavior Float
+--          by1 = Beh (\t -> y1)
+--          by2 :: Behavior Float
+--          by2 = Beh (\t -> y2)
+--          by3 :: Behavior Float
+--          by3 = Beh (\t -> y3)
 
